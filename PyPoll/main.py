b3bf35fc1dd6351c22c4b8e387a07e6895298b73
Judith Samuels <fromjandp@aol.com>
2021-01-08 03:45:54 -0600

    Try at Pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,6 @@ import os
 # Import the module to read csv files in the program
 import csv
 
-#from csv import DictReader
 # Initialize dictionary to lists to be used to hold values during the program
 politician_info = {}
 
@@ -12,6 +11,18 @@ politician_info = {}
 voter_id = []
 county = []
 candidate = []
+k = str("Khan")
+
+
+
+# Variables to calculate totals
+candidate_name = str (" ")
+total_votes = int(0)
+khan_total_votes = int(0)
+correy_total_votes = int(0)
+li_total_votes = int(0)
+otooley_total_votes = int(0)
+
 
 # Set up the path to find the .csv file to be used with the program.
 csvpath = os.path.join("Resources", "election_data1.csv")
@@ -21,12 +32,38 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
         csvreader = csv.DictReader(csvfile, delimiter=",")
-    
+        # load up the dictionary of lists    
         politician_info = {
             voter_id.append(row[0]),
             county.append(row[1]),
             candidate.append(row[2])
-            }
-    print(voter_id + county + candidate)
+        }
+        # total number of votes
+        total_votes = len(voter_id) - 1
+        for name in candidate:
+            candidate_name = name    
+            if candidate_name =="Khan" :
+                khan_total_votes =+ 1
+                print("K= " + str(khan_total_votes))
+            elif candidate_name == "Correy" :
+                    correy_total_votes =+ 1
+                    print("C = " + str(correy_total_votes))
+            elif candidate_name == "Li" :dir
+                    li_total_votes =+ 1
+                    print("L = " + str(li_total_votes))
+            else:
+                if candidate_name == "O'Tooley" :
+                    otooley_total_votes =+ 1   
+                    print("O = " + str(otooley_total_votes))     
 
+      
+        
 
+# Print the results to the screen
+print(f'Election Results')
+print(f'--------------------------------')
+print(f'Total Votes: {total_votes}')  
+print(f'--------------------------------')
+print(f'--------------------------------')
+print(f'--------------------------------')
+    
